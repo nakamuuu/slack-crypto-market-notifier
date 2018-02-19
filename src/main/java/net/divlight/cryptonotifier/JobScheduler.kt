d07cecac287caf6be2bd6f58c5bba2bc1fffdaa8
nakamuuu <keita111.mpgnt700.programming@gmail.com>
2018-02-19 22:14:56 +0900

    Fix scheduling so that scheduled execution works well

--- src/main/java/net/divlight/cryptonotifier/JobScheduler.kt
+++ src/main/java/net/divlight/cryptonotifier/JobScheduler.kt
@@ -4,21 +4,34 @@ import org.quartz.CronScheduleBuilder.dailyAtHourAndMinute
 import org.quartz.JobBuilder.newJob
 import org.quartz.TriggerBuilder.newTrigger
 import org.quartz.impl.StdSchedulerFactory
+import java.util.*
 
 open class JobScheduler {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            StdSchedulerFactory.getDefaultScheduler().run {
-                start()
-                scheduleJob(
-                    newJob(NotifierJob::class.java).build(),
-                    newTrigger().startNow()
-                        .withSchedule(dailyAtHourAndMinute(0, 0))
-                        .withSchedule(dailyAtHourAndMinute(12, 0))
-                        .build()
-                )
-            }
+            val scheduler = StdSchedulerFactory.getDefaultScheduler()
+            scheduler.start()
+            scheduler.scheduleJob(
+                newJob(NotifierJob::class.java).build(),
+                buildDailyAtHourAndMinuteTriggerWithJST(0, 0)
+            )
+            scheduler.scheduleJob(
+                newJob(NotifierJob::class.java).build(),
+                buildDailyAtHourAndMinuteTriggerWithJST(6, 0)
+            )
+            scheduler.scheduleJob(
+                newJob(NotifierJob::class.java).build(),
+                buildDailyAtHourAndMinuteTriggerWithJST(12, 0)
+            )
+            scheduler.scheduleJob(
+                newJob(NotifierJob::class.java).build(),
+                buildDailyAtHourAndMinuteTriggerWithJST(18, 0)
+            )
         }
+
+        private fun buildDailyAtHourAndMinuteTriggerWithJST(hour: Int, minute: Int) = newTrigger()
+            .withSchedule(dailyAtHourAndMinute(hour, minute).inTimeZone(TimeZone.getTimeZone("GMT+9:00")))
+            .build()
     }
 }
